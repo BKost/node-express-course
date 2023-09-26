0755989db5024f8bef424fb5b9c706dec5f32ba0
Bohdan <kosturik.bohdan@gmail.com>
2023-09-26 08:27:02 +0200

    finished first project

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,34 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasksRouter = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+// static
+app.use(express.static("./public"));
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasksRouter);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(port, () => {
+      console.log(`Server runnin ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
