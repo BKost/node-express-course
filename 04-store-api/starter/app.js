9dd74e8fa0db794c5cb9bd30a7391a05ba027fce
Bohdan <kosturik.bohdan@gmail.com>
2023-10-07 12:20:52 +0200

    Back to work

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,39 @@
-console.log('04 Store API')
+require("dotenv").config();
+// async errors
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/not-found");
+
+//middleware
+app.use(express.json());
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products</a>");
+});
+
+app.use("/api/v1/products", productsRouter);
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+//products routes
+
+const port = process.env.PORT || 4000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Port runnin on ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,37 @@
+const Product = require("../models/product.js");
+
+const getAllProductsStatic = async (req, res) => {
+  try {
+    const products = await Product.find({
+      name: "vase table",
+    });
+    res.status(200).json({ products, nbHits: products.length });
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
+const getAllProducts = async (req, res) => {
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = name;
+  }
+
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
+
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = { getAllProductsStatic, getAllProducts };
