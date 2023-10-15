331f6b4545c91140d0c77d403dbcf25580bcc137
Bohdan <kosturik.bohdan@gmail.com>
2023-10-15 16:34:32 +0200

    10.15.23 second

---
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+const { StatusCodes } = require("http-status-codes");
+
+const CustomError = require("../errors");
+
+const uploadProductImage = async (req, res) => {
+  console.log(req.files);
+
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No File Upload");
+  }
+
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please Upload Image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please Upload Image Smaller than 1KB"
+    );
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+module.exports = {
+  uploadProductImage,
+};
