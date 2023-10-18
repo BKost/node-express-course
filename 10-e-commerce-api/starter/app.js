f93f52c117b6105f0bf6b1ba1e4fc63795f57eec
Bohdan <kosturik.bohdan@gmail.com>
2023-10-18 13:57:38 +0200

    10.18.23

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,43 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+
+// other packages
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+// db
+const connectDB = require("./db/connect");
+
+// routers
+const authRouter = require("./routes/authRoutes");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+// app.post("/api/v1/auth/register", (req, res) => {
+//   console.log(req.signedCookies);
+//   res.send("eom");
+// });
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`App is listening on port ${port}`));
+  } catch (error) {}
+};
+
+start();
---
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -0,0 +1,1 @@
+const getAllUsers = async (req, res) => {};
